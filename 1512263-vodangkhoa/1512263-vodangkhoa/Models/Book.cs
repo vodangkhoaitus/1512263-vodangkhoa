04a77b5d928a2a57bcbbcdc82b495838d547d43e
1512263 <1512263@student.hcmus.edu.vn>
2019-01-05 10:31:12 +0700

    --push project

---
+++ 1512263-vodangkhoa/1512263-vodangkhoa/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+    
+    }
+}
